297dd728bc7c02ad409a73cabe5b65e0060a856f
Zi Yen Chang <ziyenchang@pc-69-224.customer.ask4.lan>
2022-12-17 22:03:48 +0000

    show under development

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,18 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <MainPage />
 </template>
 
-<style scoped>
+<script>
+import MainPage from './components/MainPage.vue';
+
+export default{
+  components:{
+    MainPage
+  },
+}
+</script>
+
+<style>
 .logo {
   height: 6em;
   padding: 1.5em;
---
+++ src/components/MainPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Zi Yen's website</h1>
+  <p>under development</p>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+</script>
+
+<style scoped>
+</style>
